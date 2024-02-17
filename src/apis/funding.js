8c01e5201f394b5d83baa8ce39443dcfa5608e10
torong-dev <hi_ryuji@naver.com>
2024-02-17 07:11:09 +0000

    Test: 펀딩 추가 API

--- src/apis/funding.js
+++ src/apis/funding.js
@@ -7,7 +7,7 @@ export const postFundingCreate = async (fundingData) => {
     const response = await instance.post("/api/funding/create", fundingData);
 
     if (response.status === 200 || response.status === 201) {
-      successToast(response.message);
+      successToast(response.msg);
       return response.data;
     }
   } catch (error) {
