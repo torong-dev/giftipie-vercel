2cd9f4542d3ec4de02ae6d21c06e3827b2118502
torong-dev <hi_ryuji@naver.com>
2024-02-18 14:39:32 +0000

    Feat: 내 펀딩 데이터를 가져오는 API

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,29 +1,28 @@
 import { instance } from "./auth";
 
 // 내 펀딩 데이터를 가져오는 API
-// export const getMyFunding = async () => {
-//   try {
-//     const response = await instance.get("/api/funding");
+export const getMyFunding = async () => {
+  try {
+    const response = await instance.get("/api/funding/myFunding");
 
-//     if (response.status === 200) {
-//       console.log("내 펀딩 정보를 가져오는 API 호출 성공: ", response);
-//       return response.data;
-//     }
-//   } catch (error) {
-//     console.error("API 호출 중 에러 발생: ", error);
+    if (response.status === 200) {
+      console.log("내 펀딩 정보를 가져오는 API 호출 성공: ", response);
+      return response.data;
+    }
+  } catch (error) {
+    console.error("API 호출 중 에러 발생: ", error);
 
-//     if (error.response) {
-//       const status = error.response.status;
-//       if (status === 404) {
-//         console.error("API 호출 중 404 에러 발생: ", error);
-//       } else if (status === 500) {
-//         console.error("API 호출 중 500 에러 발생: ", error);
-//       }
-//     }
+    if (error.response) {
+      const status = error.response.status;
+      if (status === 401) {
+        console.log(error.response.data);
+        console.error("API 호출 중 401 에러 발생: ", error);
+      }
+    }
 
-//     return null; // 에러 발생 시 데이터를 반환하지 않도록 수정
-//   }
-// };
+    return null; // 에러 발생 시 데이터를 반환하지 않도록 수정
+  }
+};
 
 // 메인페이지에서 펀딩 리스트 데이터를 가져오는 API
 export const getHomeFundingList = async () => {
