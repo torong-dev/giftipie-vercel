3b551c37836a7f25c2d22a185a861bb69d4b6532
torong-dev <hi_ryuji@naver.com>
2024-02-27 19:59:06 +0000

    Test: SSE 무한루프

--- src/components/NotificationComponent.jsx
+++ src/components/NotificationComponent.jsx
@@ -10,6 +10,9 @@ function NotificationComponent() {
     // 이미 연결되어 있거나 로그인 상태가 아니면 아무것도 하지 않습니다.
     if (isConnected || !isLoggedIn) return;
 
+    // 연결 상태를 true로 설정합니다.
+    setIsConnected(true);
+
     eventSource.current = new EventSource(
       `${process.env.REACT_APP_API_URL}/api/notification/subscribe`,
       { withCredentials: true }
@@ -17,7 +20,6 @@ function NotificationComponent() {
 
     eventSource.current.onopen = () => {
       console.log("SSE Connection opened.");
-      setIsConnected((prevIsConnected) => !prevIsConnected); // 이전 상태값을 이용하여 변경
     };
 
     eventSource.current.onmessage = (event) => {
@@ -27,18 +29,20 @@ function NotificationComponent() {
     eventSource.current.onerror = (error) => {
       console.error("SSE Connection error:", error);
       eventSource.current.close();
-      setIsConnected((prevIsConnected) => !prevIsConnected); // 이전 상태값을 이용하여 변경
+      // 연결 상태를 false로 설정합니다.
+      setIsConnected(false);
     };
 
     // 컴포넌트 언마운트 또는 로그아웃 시 SSE 연결을 종료합니다.
     return () => {
       if (eventSource.current) {
         eventSource.current.close();
-        setIsConnected((prevIsConnected) => !prevIsConnected); // 이전 상태값을 이용하여 변경
+        // 연결 상태를 false로 설정합니다.
+        setIsConnected(false);
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [isLoggedIn]); // isConnected를 의존성 배열에서 제거합니다.
 
   return <img src="/imgs/Home/no-notification.svg" alt="notification" />;
 }
