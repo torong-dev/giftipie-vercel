0e34259736820eaa48c7177062392446e70325e2
torong-dev <hi_ryuji@naver.com>
2024-02-29 21:23:23 +0000

    Test: 실시간 알림 아이콘 변경

--- src/components/NotificationComponent.jsx
+++ src/components/NotificationComponent.jsx
@@ -1,3 +1,69 @@
+// import React, { useEffect, useState, useRef } from "react";
+// import { useSelector } from "react-redux";
+// import { infoToast } from "./toast";
+// import { ToastContainer } from "react-toastify";
+
+// function NotificationComponent() {
+//   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+//   const eventSource = useRef(null);
+//   const [isConnected, setIsConnected] = useState(false);
+
+//   useEffect(() => {
+//     const initializeEventSource = () => {
+//       eventSource.current = new EventSource(
+//         `${process.env.REACT_APP_API_URL}/api/notification/subscribe`,
+//         {
+//           withCredentials: true,
+//         }
+//       );
+
+//       eventSource.current.onopen = () => {
+//         console.log("SSE Connection opened.");
+//         setIsConnected(true);
+//       };
+
+//       eventSource.current.addEventListener("sse", (event) => {
+//         const data = JSON.parse(event.data);
+//         console.log("SSE data received:", data);
+//         infoToast(data.message);
+//       });
+
+//       eventSource.current.onerror = () => {
+//         console.error("SSE Connection error");
+//         if (eventSource.current) eventSource.current.close();
+//         setIsConnected(false);
+
+//         setTimeout(() => {
+//           if (isLoggedIn && !isConnected) {
+//             initializeEventSource();
+//           }
+//         }, 3000);
+//       };
+//     };
+
+//     if (isLoggedIn && !isConnected) {
+//       initializeEventSource();
+//     }
+
+//     return () => {
+//       if (eventSource.current) {
+//         eventSource.current.close();
+//         setIsConnected(false);
+//       }
+//     };
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [isLoggedIn]);
+
+//   return (
+//     <div>
+//       <img src="/imgs/Home/no-notification.svg" alt="notification" />
+//       <ToastContainer />
+//     </div>
+//   );
+// }
+
+// export default NotificationComponent;
+
 import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { infoToast } from "./toast";
@@ -6,7 +72,7 @@ import { ToastContainer } from "react-toastify";
 function NotificationComponent() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const eventSource = useRef(null);
-  const [isConnected, setIsConnected] = useState(false);
+  const [unreadNotification, setUnreadNotification] = useState(false);
 
   useEffect(() => {
     const initializeEventSource = () => {
@@ -19,36 +85,28 @@ function NotificationComponent() {
 
       eventSource.current.onopen = () => {
         console.log("SSE Connection opened.");
-        setIsConnected(true);
       };
 
       eventSource.current.addEventListener("sse", (event) => {
         const data = JSON.parse(event.data);
         console.log("SSE data received:", data);
         infoToast(data.message);
+        setUnreadNotification(true);
       });
 
       eventSource.current.onerror = () => {
         console.error("SSE Connection error");
         if (eventSource.current) eventSource.current.close();
-        setIsConnected(false);
-
-        setTimeout(() => {
-          if (isLoggedIn && !isConnected) {
-            initializeEventSource();
-          }
-        }, 3000);
       };
     };
 
-    if (isLoggedIn && !isConnected) {
+    if (isLoggedIn) {
       initializeEventSource();
     }
 
     return () => {
       if (eventSource.current) {
         eventSource.current.close();
-        setIsConnected(false);
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -56,7 +114,11 @@ function NotificationComponent() {
 
   return (
     <div>
-      <img src="/imgs/Home/no-notification.svg" alt="notification" />
+      {unreadNotification ? (
+        <img src="/imgs/Home/notification.svg" alt="notification" />
+      ) : (
+        <img src="/imgs/Home/no-notification.svg" alt="notification" />
+      )}
       <ToastContainer />
     </div>
   );
