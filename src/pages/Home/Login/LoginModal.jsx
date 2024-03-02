c6fccfc9b3a9d40937308d30f3522c0641a585cd
torong-dev <hi_ryuji@naver.com>
2024-03-02 12:06:17 +0000

    Test: 카카오 로그인

--- src/pages/Home/Login/LoginModal.jsx
+++ src/pages/Home/Login/LoginModal.jsx
@@ -38,7 +38,8 @@ const LoginModal = ({ closeModal }) => {
   const KakaoLogin = async () => {
     const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
     const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
-    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const code = new URL(document.location.toString()).searchParams.get("code");
+    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=${code}`;
 
     try {
       await postKakaoLogin();
