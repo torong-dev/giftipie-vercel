db8054fcc513ec7dd32b6db31058f1e7dd59bcc0
torong-dev <hi_ryuji@naver.com>
2024-03-05 15:53:54 +0000

    Test: 후원 결제실패

--- src/pages/Callbacks/KakaoPayFail.jsx
+++ src/pages/Callbacks/KakaoPayFail.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getDonationFail } from "../../apis/funding";
+import { BarLoader } from "react-spinners";
+import { SpinnerContainer } from "./CallbacksStyle";
+import theme from "../../styles/theme";
+import { successToast } from "../../components/toast";
 
 const KakaoPayFail = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  // 후원 결제실패 API 호출
+  const getData = async () => {
+    try {
+      if (window.location.href.includes("cancel")) {
+        const data = await getDonationFail();
+
+        if (data.isSuccess === true) {
+          navigate(`/fundingdetail/${data.result}`);
+          successToast(data.message);
+        }
+      }
+    } catch (error) {
+      console.error("결제실패 오류: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <SpinnerContainer>
+      <BarLoader color={theme.primary} loading={true} />
+    </SpinnerContainer>
+  );
 };
 
 export default KakaoPayFail;
